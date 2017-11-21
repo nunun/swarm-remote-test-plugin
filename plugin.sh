6c55d0aca2a2a69e992209278c938eec89e68a67
nunun <hoge.life@gmail.com>
2017-11-21 13:27:52 +0900

    update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,12 +1,15 @@
-
 on_install() {
-        echo "swarm-plugin-test: on_install"
+        echo "swarm-remote-test-plugin: on_install (${*})"
 }
 
 on_uninstall() {
-        echo "swarm-plugin-test: on_uninstall"
+        echo "swarm-remote-test-plugin: on_uninstall (${*})"
 }
 
 on_update() {
-        echo "swarm-plugin-test: on_update"
+        echo "swarm-remote-test-plugin: on_update (${*})"
+}
+
+on_compose() {
+        echo "swarm-remote-test-plugin: on_compose (${*})"
 }
